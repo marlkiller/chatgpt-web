58654c4f62781253a042ce8049659961d87ae3b3
artemis <marlkiller@voidm.com>
2023-03-02 16:42:57 +0800

    Add: add operator contact

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,8 +15,8 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
+      avatar: 'https://avatars.githubusercontent.com/u/25897113?v=4',
+      name: '',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
